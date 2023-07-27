4eaaa6f94749104c9cdb474353f79cd6694fa649
Maksym-Feshchenko <maksym.fd@gmai.com>
2023-07-27 22:27:06 +0300

    messages с_schema

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -4,8 +4,12 @@ const contactAddSchema = Joi.object({
     name: Joi.string().required().messages({
       "any.required": 'missing required name field'
     }),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
+    email: Joi.string().required().messages({
+      "any.required": 'missing required email field'
+    }),
+    phone: Joi.string().required().messages({
+      "any.required": 'missing required phone field'
+    }),
     favorite: Joi.boolean()
 })
 
