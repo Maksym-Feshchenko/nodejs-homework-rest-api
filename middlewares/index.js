f40e88c055c07bbf915f06a6fb42fd0180a82a36
Maksym-Feshchenko <maksym.fd@gmai.com>
2023-08-07 21:08:13 +0300

    middleware upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,7 +3,6 @@ export {default as isValidId} from "./isValidId.js";
 export {default as isEmptyBodyFavorite} from "./isEmptyBodyFavorite.js";
 export {default as authenticate} from "./authenticate.js";
 export {default as upload} from "./upload.js";
-export {default as avatarProcessor} from "./upload.js";
 
 
 
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -7,7 +7,6 @@ import usersSchemas from "../../schemas/users-schemas.js";
 import {validateBody} from "../../decorators/index.js"; 
 
 import {authenticate, upload} from "../../middlewares/index.js"
-// import avatarProcessor from "../../middlewares/upload.js"
 
 const authRouter = express.Router();
 
@@ -19,7 +18,6 @@ authRouter.get("/current", authenticate, authController.getCurrent)
 
 authRouter.post("/logout", authenticate, authController.logout);
 
-authRouter.patch("/avatars", authenticate, upload.single("avatar"), authController.updateAvatar)
-// authRouter.patch("/avatars", authenticate, upload.single("avatar"), avatarProcessor, authController.updateAvatar);
+authRouter.patch("/avatars", authenticate, upload, authController.updateAvatar)
 
 export default authRouter;
